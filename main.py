35e6271a49c359a13c5dd2957e64a755962ec770
Piyush Bhatore <piyushbhatore@gmail.com>
2017-07-12 10:17:40 +0530

    predict type of wine

---
+++ main.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import plot_model
+from keras.utils.vis_utils import model_to_dot
+from IPython.display import SVG
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import f1_score
+
+red = pd.read_csv('./winequality-red.csv',sep=';')
+white = pd.read_csv('./winequality-white.csv',sep=';')
+red['type'] = 1
+white['type'] = 0
+wines = red.append(white,ignore_index=True)
+X = wines.ix[:,0:11]
+y = wines['type']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2)
+scaler = StandardScaler().fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = Sequential()
+
+model.add(Dense(12,activation='relu',input_shape = (11,)))
+model.add(Dense(1,activation='sigmoid'))
+
+model.summary()
+##printing model
+SVG(model_to_dot(model).create(prog='dot', format='svg'))
+plot_model(model, to_file='model.png', show_shapes=True)
+##fitting
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+                   
+model.fit(X_train, y_train,epochs=10, batch_size=1, verbose=1)
+y_pred = model.predict(X_test)
+y_pred = y_pred > 0.5
+y_pred = y_pred.astype(int)
+print(f1_score(y_test,y_pred))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Model config
+#print(model.get_weights())
+
+# # List all weight tensors 
+# model.get_weights()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# fig, ax = plt.subplots(1, 2)
+
+# ax[0].hist(red.alcohol, 10, facecolor='red', alpha=0.5, label="Red wine")
+# ax[1].hist(white.alcohol, 10, facecolor='white', ec="black", lw=0.5, alpha=0.5, label="White wine")
+
+# fig.subplots_adjust(left=0.09, right=1, bottom=0.09, top=0.9, hspace=0.05, wspace=1)
+# ax[0].set_ylim([0, 1000])
+# ax[0].set_xlabel("Alcohol in % Vol")
+# ax[0].set_ylabel("Frequency")
+# ax[1].set_xlabel("Alcohol in % Vol")
+# ax[1].set_ylabel("Frequency")
+
+# #ax[0].legend(loc='best')
+# #ax[1].legend(loc='best')
+# fig.suptitle("Distribution of Alcohol in % Vol")
+
+# plt.show()
